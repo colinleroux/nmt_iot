01ed6a8cb1dadd405dfdb53acbfacf494dc9f0a1
Colin le Roux <colinleroux@gmail.com>
2023-03-23 17:50:04 +0800

    added authentication to API

--- api/main.py
+++ api/main.py
@@ -1,8 +1,28 @@
+import casbin
 from fastapi import FastAPI
+from fastapi_authz import CasbinMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
 
+import basic_auth
 app = FastAPI()
+enforcer = casbin.Enforcer(
+   'rbac_model.conf',
+   'rbac_policy.csv',
+)
+backend = basic_auth.BasicAuth()
+
+app.add_middleware(CasbinMiddleware, enforcer=enforcer)
+app.add_middleware(AuthenticationMiddleware, backend=backend)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def index():
+   return {"message": "Hello World"}
+
+@app.get("/ds1/res1")
+async def ds1_res1():
+    return "Greetings from the ds1/res1 endpoint"
+
+@app.get("/ds2/res2")
+async def ds2_res2():
+    return "Helloa from the ds2/res2 endpoint"
